7c82f846d31f61a5f61e6357ac3cb78c160b2525
KuPun <trangiathanh0205@gmail.com>
2025-01-26 22:23:21 +0700

    L36-5

--- L36-DOM/main.js
+++ L36-DOM/main.js
@@ -1,16 +1,11 @@
-// 1. getElementsByTagName
-console.log("getElementsByTagName: ");
-let li_item = document.getElementsByTagName("li"); // 2. return an array of li elements
-console.log(li_item); // To check the number of elements in an array.
+function updateClock() {
+    const now = new Date();
+    const hour = String(now.getHours()).padStart(2, "0");
+    const minute = String(now.getMinutes()).padStart(2, "0");
+    const second = String(now.getSeconds()).padStart(2, "0");
+    const timeString =  `${hour}:${minute}:${second}`;
 
-li_item[0].innerHTML = "aaaaaa"; // 3. To retrieve and adjust a specific element.
+    document.getElementById("clock").innerHTML = timeString;
+}
 
-// 4. getElementsByClassName
-console.log("getElementsByClassName");
-let li_class_item = document.getElementsByClassName("list");
-console.log(li_class_item);
-
-// 5. 
-console.log("getElementById");
-let li_id_item = document.getElementById("list_about3");
-console.log(li_id_item);
+setInterval(updateClock, 1000);
