efe94b013091dac96e5c3d3ea0f7cd712d6bc175
KuPun <trangiathanh0205@gmail.com>
2025-01-26 23:33:34 +0700

    L36-6

--- L36-DOM/main.js
+++ L36-DOM/main.js
@@ -1,11 +1,34 @@
-function updateClock() {
-    const now = new Date();
-    const hour = String(now.getHours()).padStart(2, "0");
-    const minute = String(now.getMinutes()).padStart(2, "0");
-    const second = String(now.getSeconds()).padStart(2, "0");
-    const timeString =  `${hour}:${minute}:${second}`;
+// 8. getAttribute
+// Sử dụng querySelector để chọn thẻ a đầu tiên.
+const firstLink = document.querySelector("a");
+// console.log (firstLink);
 
-    document.getElementById("clock").innerHTML = timeString;
+if (firstLink) {
+    const linkHref = firstLink.getAttribute("href");
+    console.log(linkHref); 
+}
+else {
+    console.log("Không tìm thấy thẻ a");
 }
 
-setInterval(updateClock, 1000);
+// 9. setAttribute
+// "target", "_blank": dùng để mở trang mới.
+firstLink.setAttribute("target", "_blank");
+
+// Sử dụng querySelectorAll để chọn tất cả các thẻ a trong ul.
+const links = document.querySelectorAll("ul a");
+console.log(links);
+// Lập qua mỗi thẻ a và sử dụng setAttribute
+// để thêm thuộc tính target="_blank"
+links.forEach((singleLink) => {
+    singleLink.setAttribute("target", "_blank");
+});
+
+// Bài tập đổi red thành blue.
+const firstStyle = document.querySelector("h1");
+// console.log(firstStyle);
+
+// const firstColor = firstStyle.getAttribute("style");
+// console.log(firstColor);
+
+firstStyle.setAttribute("style", "color: blue;");
