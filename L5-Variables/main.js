cfd12cda606412aeef9179cd3729ac2f539f04eb
KuPun <trangiathanh0205@gmail.com>
2025-01-26 18:44:51 +0700

    add L5

---
+++ L5-Variables/main.js
@@ -0,0 +1,19 @@
+// alert("Hello");
+// prompt("Please enter your full name:");
+
+// var diemToan = 10;
+// let firstName = "Tan";
+
+// console.log("Điểm Toán của bạn là 10 điểm: " + diemToan);
+let username = prompt("Mời bạn nhập tên: ")
+console.log("Tên của bạn là: " + username);
+// Cách phổ thông:
+console.log("Xin chào mừng " + username + " đến với trang facebook");
+
+let a = 25;
+let b = 5;
+
+// Cách 1: 
+console.log("Căn bậc hai của %s là: %s", a, b);
+// Cách 2:
+console.log(`Căn bậc hai của ${a} là: ${b}`);
